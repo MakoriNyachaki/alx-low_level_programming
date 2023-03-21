b8ad9945cc615549a54070258e299b95fe1b8822
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 07:27:29 -0700

    Print the alphabet 10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet 10 times in lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
