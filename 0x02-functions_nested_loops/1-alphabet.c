37f5648d9264b1ec697a99e9a4f8fe7f4ad2799b
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 06:29:55 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - Prints the alphabet in lowercase followed by newline
  * using _putchar
- * print_alphabet: prints a-z
+ * print_alphabet(): prints a-z
  * Return: Always 0 success
  */
 
@@ -10,5 +10,18 @@ void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
+	return (0);
+}
+void print_alphabet(voi)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
