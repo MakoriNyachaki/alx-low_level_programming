238c11a441f9f0b6216c4b72277df5f818ce56f7
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 09:40:51 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#inclued "main.h"
+/**
+ * print_sign - Prints the sign of a number
+ * @n: number whose sign to be found
+ * Return: 1 if +, 0 if 0 and -1 if _
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	return (-1);
+}
