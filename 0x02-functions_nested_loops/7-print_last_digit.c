bef0af057b36f0455838a6aee356760ca9c3f1c1
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 10:29:27 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the las digit of a number
+ * @m: integer whose last digit to be found
+ * Return: returns the last digit l
+ */
+int print_last_digit(int m)
+{
+	int l;
+
+	l = m % 10;
+	return (l);
+}
