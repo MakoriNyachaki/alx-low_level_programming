048a00893e355515ef87f83dab19c5ddc6b326f7
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 11:44:46 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @k: one of the integers
+ * @l: Integer
+ * Return: returns the value of the sum
+ */
+int add(int k, int l)
+{
+	int sum;
+
+	sum = k + l;
+	return (sum);
+}
