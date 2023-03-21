28eeb047fef480d7a8fcb27fc4b6cdb22711ab49
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 10:20:27 -0700

    Absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
  * _abs - prints an absolute value
- * @int: numbers whose absolute value is being found
+ * @k: numbers whose absolute value is being found
  * Return: 0 success
  */
-int _abs(int)
+int _abs(int k)
 {
-	int k;
-
 	return ((k < 0) ? -k : k);
 }
