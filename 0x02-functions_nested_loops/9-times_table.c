2218920e5824e11ad0a5875be478bc49c97462b7
Makori Nyachaki <nyachakim@gmail.com>
2023-03-21 20:15:36 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,18 +5,16 @@
 void times_table(void)
 {
 	int i, j;
-	long int r;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			r = i * j;
 			_putchar((i / 10) + '0');
 			_putchar((j / 10) + '0');
 			_putchar(',');
 			_putchar(' ');
-			_putchar(r + '0');
+			_putchar((i * j) + '0');
 			_putchar('\n');
 		}
 	}
