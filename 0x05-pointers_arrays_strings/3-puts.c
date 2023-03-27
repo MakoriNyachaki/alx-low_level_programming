21d54abdafcec19dd158a8994a1e5284e209eb53
Makori Nyachaki <nyachakim@gmail.com>
2023-03-27 07:52:30 -0700

    Return character followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * _puts - prints a character the followed by a new line
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	int k;
+
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		_puchar(str[k]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - returns character
+ * @c: character returned
+ * Return: character
+ */
+int _putchar(char c)
+{
+	return (1, &c, 1);
+}
