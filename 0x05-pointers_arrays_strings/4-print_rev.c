4dc234afa660edc82c0e87e123c71ccfb3391999
Makori Nyachaki <nyachakim@gmail.com>
2023-03-27 08:06:00 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int m, n, len;
+
+	i = 0;
+
+	while (s[m] != '\0')
+	{
+		m++;
+	}
+
+	len = m;
+
+	for (n = len - 1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
