75be354c68f748f3804da740e5002f0fa928c7c5
Makori Nyachaki <nyachakim@gmail.com>
2023-03-27 07:29:27 -0700

    String lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - finds the lenght of a string
+ * @s: string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char *i = s;
+	
+	while (*p != '\0')
+	{
+		p++;
+	}
+	return i - s;
+}
