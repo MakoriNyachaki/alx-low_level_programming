60bef3f09c829ce0625226a6de1774772d4d6521
Makori Nyachaki <nyachakim@gmail.com>
2023-03-30 05:16:02 -0700

    Encode using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes using rot13
+ * @s: string to be encoded
+ *
+ * Return: rsesult
+ */
+
+char *rot13(char *s)
+{
+	int c, d;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] =  "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		for (d = 0; a[d] != '\0'; d++)
+		{
+			if (s[c] == a[d])
+			{
+				s[c] = b[d];
+				break;
+			}
+		}
+	}
+	return (s);
+}
