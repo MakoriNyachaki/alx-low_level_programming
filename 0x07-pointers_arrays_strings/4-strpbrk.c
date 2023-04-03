33bacb321a5696173682a4939db541eaca9ca7b8
Makori Nyachaki <nyachakim@gmail.com>
2023-04-03 10:41:34 -0700

    Search string for any number of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - Searches any set bytes in a string
+ * @s: string to search
+ * @accept: String which contain the bytes
+ * Return: s or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+}
