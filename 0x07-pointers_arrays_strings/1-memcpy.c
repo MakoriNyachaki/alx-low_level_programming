41553aea50073cad20226e82b33a75b184b00f5d
Makori Nyachaki <nyachakim@gmail.com>
2023-04-03 08:10:18 -0700

    Memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - Copies memory area
+ * @dest: pointer the memory area
+ * @src: input source
+ * @n: memory size to copy to
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char a = dest;
+
+	while (n > 0)
+	{
+		*dest = *src;
+		n--;
+	}
+	return (dest);
+}
