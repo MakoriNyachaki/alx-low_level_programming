0ff50200ec4390477fd28dd1047d220a1362e029
Makori Nyachaki <nyachakim@gmail.com>
2023-04-04 19:39:27 -0700

    String containing bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,4 +9,20 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int m, n;
+
+	m = 0;
+	n = 0;
+
+	for (; *s != '\0'; m++)
+	{
+		while (accept[n] != '\0')
+		{
+			if (*s == accept[n])
+				return (s);
+			n++;
+		}
+		s++;
+	}
+	return (NULL);
 }
