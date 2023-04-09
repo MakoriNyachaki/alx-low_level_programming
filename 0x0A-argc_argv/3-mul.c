4a43f0a71bf839934bc8e0011e838fdad1b66af7
Makori Nyachaki <nyachakim@gmail.com>
2023-04-09 09:45:29 -0700

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,50 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * atoi - converts a string to an integer
+ * @s: string
+ * Return: integer
+ */
+int atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+
 /**
  * main - funds the result of a multiplication
  * @argc: number of arguments
@@ -18,8 +63,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		num1 = _atoi(argv[1]);
-		num2 = _atoi(argv[2]);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
 
 		res = num1 * num2;
 		printf("%d\n", res);
