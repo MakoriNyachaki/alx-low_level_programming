24496fae7d50fdbb2641263b242809215cde4f3b
Makori Nyachaki <nyachakim@gmail.com>
2023-04-09 07:50:41 -0700

    What is my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: Always 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("my name is: %s\n", argv[0]);
+	return (0);
+}
