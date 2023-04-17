a13aafc48d71803af935c8a07e28c79f648c71e1
Makori Nyachaki <nyachakim@gmail.com>
2023-04-17 08:05:55 -0700

    1 debugged

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,13 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != 0)
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	else
 	{
 		d->name = name;
 		d->age = age;
