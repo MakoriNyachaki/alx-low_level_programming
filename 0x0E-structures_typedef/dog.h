364c60c610c07a9992291cf2732305aa2d9d7229
Makori Nyachaki <nyachakim@gmail.com>
2023-04-17 07:06:31 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -25,14 +25,14 @@ struct dog
 
 struct dog *my_dog(char *name, float age, char *owner)
 {
-	struct dog *my_dog;
+	struct dog *Dog;
 
-	my_dog = malloc(sizeof(struct dog));
+	Dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-	return (my_dog);
+	Dog->name = name;
+	Dog->age = age;
+	Dog->owner = owner;
+	return (Dog);
 }
 #endif
