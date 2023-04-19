4a3b1be23078c2f2055d8ff7f27e6730ede45a24
Makori Nyachaki <nyachakim@gmail.com>
2023-04-19 03:05:53 -0700

    Searches for an int debugged 5

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,13 +18,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != NULL && cmp != NULL)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
 		while (i < size)
 		{
 			if (cmp(array[i] != 0))
 				return (i);
-			return (-1);
 			i++;
 		}
 	}
