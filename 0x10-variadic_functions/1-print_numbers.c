92f33da74db13bc02577aa9c42d090c53b5fe1cb
Makori Nyachaki <nyachakim@gmail.com>
2023-04-20 13:02:15 -0700

    Task 2 debug final

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(vl, int));
 		index++;
-		if (index == (n - 1) && separator)
+		if (index <= (n - 1) && separator)
 			printf("%s", separator);
 	}
 	va_end(vl);
