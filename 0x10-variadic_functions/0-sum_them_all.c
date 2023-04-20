75f585049c25b6490506bfc2379b6fa810bfcf64
Makori Nyachaki <nyachakim@gmail.com>
2023-04-20 03:06:50 -0700

    Task 0 debugged

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int index, add;
+	unsigned int index = 0;
+	int add = 0;
+
 	va_list la;
 
 	if (n != 0)
 	{
 		va_start(la, n);
 
-		add = 0;
-		index = 0;
-
 		while (index < n)
 		{
-			sum += va_arg(la, unsigned int);
+			sum += va_arg(la, int);
 			index++;
 		}
 		va_end(la);
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,15 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - a funtion that prints anything
+ *
+ * @formaat: list of types of arguments passed
+ * 	c:char
+ * 	i:integer
+ * 	f:float
+ * 	s:char *
+ */
+
+void print_all(const char * const format, ...)
+{
+	
