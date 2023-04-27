d6b732bf51fe94c3f55fc8d71e318347451f15c0
Makori Nyachaki <nyachakim@gmail.com>
2023-04-27 07:44:57 -0700

    Executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void makori(void) __attribute__ ((constructor));
+
+/**
+ * makori - executes first before main
+ * and it prints some strings
+ */
+
+void makori(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
