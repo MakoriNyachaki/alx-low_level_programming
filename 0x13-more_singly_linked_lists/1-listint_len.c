2d6d9d54d6615d8d7ec493622b14e4f6f94766f9
Makori Nyachaki <nyachakim@gmail.com>
2023-04-30 01:57:15 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of
+ * elements in a linked list
+ *
+ * @h: pointer to the first node
+ *
+ * Return: number of elements
+ *
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size _t nodeList = 0;
+
+	for (; h; )
+	{
+		nodeList++;
+		h = h->next;
+	}
+	return (nodeList);
+}
