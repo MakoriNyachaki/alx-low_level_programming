85ab693f9c3e1199cd5c6e51422118b20a1a8437
Makori Nyachaki <nyachakim@gmail.com>
2023-04-30 06:19:35 -0700

    Task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data of a listint_t
+ *
+ * @head: pointer to the first node
+ *
+ * Return: 0 or sum
+ *
+ */
+
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+
+	if (head)
+	{
+		while (head)
+		{
+			total = total + head->next;
+			head = head->next;
+		}
+	}
+	else
+		return (0);
+
+	return (total);
+}
