6ac19789d47317f4ee4bcd1a26d4c1ffc7946b9d
Makori Nyachaki <nyachakim@gmail.com>
2023-05-02 01:34:48 -0700

    Task 0 debugged again

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,9 +14,9 @@ size_t print_listint(cons listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", (*h).n);
-		h = h + 1;
-		nodeList++;
+		printf("%d\n", (*h)->n);
+		h = h->next;
+		nodeList += (h != NULL) ? 1 : 0;
 	}
 	return (nodeList);
 }
