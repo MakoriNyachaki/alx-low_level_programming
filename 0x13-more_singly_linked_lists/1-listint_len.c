4a9c3d0a3d61ed35622012305b7e3e7849626449
Makori Nyachaki <nyachakim@gmail.com>
2023-05-02 01:46:59 -0700

    Task 1 Debugged again

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -12,12 +12,8 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size _t nodeList = 0;
+	if (h == NULL)
+		return (0);
 
-	while (h)
-	{
-		nodeList++;
-		h = h->next;
-	}
-	return (nodeList);
+	return (1 + listint_len(h->next));
 }
