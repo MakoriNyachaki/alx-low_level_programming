bfaae5c4a2560c30476682103b1b7b37acc77339
Makori Nyachaki <nyachakim@gmail.com>
2023-05-01 23:17:30 -0700

    Task 10  debugged

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,14 +11,23 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int n;
+
 	if (*head == NULL)
 		return (-1);
-	if (index == 0)
+	listint_t *ear = NULL, *now = *head;
+
+	switch (index)
 	{
-		listint_t *t = *head;
-		*head = (*head)->next;
-		free(k);
-		return (1);
-	}
-	return (delete_nodeint_at_index(&(head)->next, index - 1));
+		case 0:
+			*head = now->next;
+			free(now);
+			return (1);
+		default:
+			for (n = 0; n < index; n++)
+			{
+				if (now == NULL)
+					return (-1);
+				ear = now;
+			}
 }
