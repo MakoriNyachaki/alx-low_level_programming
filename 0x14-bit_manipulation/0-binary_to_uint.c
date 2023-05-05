1ba8c0f5aa4745c545399dbdb3394d2113d45e46
Makori Nyachaki <nyachakim@gmail.com>
2023-05-04 22:29:03 -0700

    Task 0 debugged

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,21 +12,6 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int out = 0;
-
-	if (b == NULL)
-		return (0);
-	while (b != '\0')
-	{
-		if (*b == '0' || *b == '1')
-		{
-			out <<= 1;
-			out |= (*b - '0');
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (out);
+	return (b ? &&*b ? ((*b == '0' ? binary_to_int(++b) << 1
+					 * b == '1' ? binary_to_int(++b) << 1 | 1 : 0)) : 0);
 }
